d9a2184a4c96c947d79bf33f8f7d8a5387cf8da7
PinkieORG <473839@mail.muni.cz>
2024-04-06 04:40:06 +0200

    Made hit or miss work with binary sets.

--- game_map/areas/tiles/morphology/operations.py
+++ game_map/areas/tiles/morphology/operations.py
@@ -2,25 +2,24 @@ import numpy as np
 
 from game_map.areas.tiles.morphology.structural_element import StructuralElement
 from game_map.areas.tiles.supplementaries import Point
-from game_map.areas.tiles.tiles import Tiles
 from utils import subtract_tuples
 
 
-def hit_or_miss(in_tiles: Tiles, se: StructuralElement):
+def hit_or_miss(in_tiles, se: StructuralElement):
     """Hit or Miss morphological operation with the given structural element."""
     if not se.tiles.point_in_bbox(se.origin):
         raise ValueError(
             "The function does not support the origin outside the "
             "structuring element."
         )
-    result = Tiles(in_tiles.size, empty=True)
-    for i in np.ndindex(in_tiles.size):
+    result = np.full(in_tiles.shape, fill_value=False)
+    for i in np.ndindex(in_tiles.shape):
         o = subtract_tuples(i, (se.origin.y, se.origin.x))
-        cutout = in_tiles.mask[
+        cutout = in_tiles[
             max(0, o[0]) : o[0] + se.h,
             max(0, o[1]) : o[1] + se.w,
         ]
-        result.tiles[i] = np.all(
-            cutout == se.tiles.clipped(Point(o[0], o[1]), in_tiles.size).mask
+        result[i] = np.all(
+            cutout == se.tiles.clipped(Point(o[0], o[1]), in_tiles.shape).mask
         )
     return result
