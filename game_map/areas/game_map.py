004e2ea7ed3dbcdb7fd20c2d9dc2bb23fd398666
PinkieORG <473839@mail.muni.cz>
2024-04-06 03:01:26 +0200

    To Simple Area

--- game_map/areas/game_map.py
+++ game_map/areas/game_map.py
@@ -5,7 +5,7 @@ import tile_types
 from game_map.areas.area import Area
 from game_map.areas.tiles.supplementaries import Point
 from game_map.direction.direction import Direction
-from game_map.areas.rooms.rooms import LRoom
+from game_map.areas.rooms.rooms import LRoom, prototype_room
 
 
 class GameMap(Area):
@@ -20,15 +20,15 @@ def generate_dungeon(map_height, map_width) -> GameMap:
 
     game_map = GameMap((map_height, map_width))
 
-    room1 = LRoom((40, 30), Direction.EAST)
-    room1.origin = Point(1, 40)
+    room1 = prototype_room((40, 30))
 
+    room1.origin = Point(1, 40)
     game_map.place_in(room1)
 
-    room2 = LRoom((20, 20), Direction.WEST)
-
-    p = game_map.fit_next_to(room2, room1, Direction.WEST)
-
-    game_map.place_in_randomly(p, room2)
+    # room2 = LRoom((20, 20), Direction.WEST)
+    #
+    # p = game_map.fit_next_to(room2, room1, Direction.WEST)
+    #
+    # game_map.place_in_randomly(p, room2)
 
     return game_map
