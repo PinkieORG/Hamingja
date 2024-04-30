422fd24d91447754ae70843937391b3a87639ac1
PinkieORG <473839@mail.muni.cz>
2024-04-30 21:23:42 +0200

    Make entrance methods. testing.

--- game_map/areas/game_map.py
+++ game_map/areas/game_map.py
@@ -3,6 +3,7 @@ from typing import Tuple
 import tile_types
 from game_map.areas.area import Area
 from game_map.areas.rooms.rooms import Room
+from game_map.areas.simple_area import SimpleArea
 from utils.graph.graph import Graph
 
 
@@ -22,3 +23,18 @@ class GameMap(Area):
             self.room_graph.push(to_place, [neighbour])
             self.room_graph.add_neighbour_to(neighbour, to_place)
         return placed
+
+    def make_entrance_between(self, first: Room, second: Room) -> None:
+        if first not in self.children or second not in self.children:
+            raise ValueError("Rooms not in children.")
+        intersection = SimpleArea.intersection(first, second)
+        entrance_point = intersection.get_random_coordination()
+        entrance = SimpleArea((1, 1), entrance_point)
+        entrance.fill(tile_types.yellow)
+        self.postfilter.append(entrance)
+        # first.make_entrance(entrance_point - first.origin)
+        # second.make_entrance(entrance_point - second.origin)
+
+    def make_entrance_room(self, room: Room) -> None:
+        for neighbour in self.room_graph.get_neighbours(room):
+            self.make_entrance_between(room, neighbour)
